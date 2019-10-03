5da4d6850e367098c1db54f8085af0133f641766
Ali Karimi <ajkarimi7@gmail.com>
2019-10-03 07:37:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,11 +26,9 @@ class Scraper
     #   course.title = post.css("h2").text
     #   course.schedule = post.css(".date").text
     #   course.description = post.css("p").text
-    end 
-    
+    # end
   end 
     
-  
 end
 
 Scraper.new.get_page
