276f54e364459ecf8f3dc54a06d58a7b87d7996a
Ali Karimi <ajkarimi7@gmail.com>
2019-10-03 07:23:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,7 +20,7 @@ class Scraper
   def get_page
     html = open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
   
-    doc = Nokogiri::HTML(html)
+    doc = Nokogiri::HTML(open())
     
     binding.pry 
   end 
